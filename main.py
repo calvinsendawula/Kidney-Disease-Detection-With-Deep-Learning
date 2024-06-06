cf4117265926db354281b340e285e8d3cbd998f7
calvinsendawula <calvin.sendawula@strathmore.edu>
2024-06-06 14:59:54 +0300

    feat: add data ingestion

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 from kidneyDiseaseClassifier import logger
+from kidneyDiseaseClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("Welcome to our custom logs!")
+STAGE_NAME = "Data Ingestion Stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
